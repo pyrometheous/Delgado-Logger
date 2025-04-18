fa0d7cc1074eb3fa5968e8334dfba10ba809e1e1
pyrometheous <Pyrometheous@gmail.com>
2025-04-18 16:47:59 -0700

    Updating to include README in PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='delgado_logger',
     version='0.1.0',
     author='Delgado',
     description='A lightweight and human-readable logging and CSV utility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # <-- THIS IS CRITICAL
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.7',
@@ -12,6 +18,6 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
     ],
-    url='https://github.com/yourusername/delgado_logger',
+    url='https://github.com/pyrometheous/Delgado-Logger',
     license='MIT'
 )
